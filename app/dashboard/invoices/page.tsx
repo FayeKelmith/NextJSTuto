d122c52b9aca2ab24c41b19a0f5fd23194260c23
Kelmith <fayekelmith@gmail.com>
2023-11-29 17:28:43 +0530

    about to revalidate

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,8 +5,15 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
-
-const Invoices = async () => {
+import { fetchInvoicesPages } from '@/app/lib/data';
+const Invoices = async ({
+  searchParams,
+}: {
+  searchParams?: { query?: string; page?: number };
+}) => {
+  const query = searchParams?.query || '';
+  const currentPage = searchParams?.page || 1;
+  const totalPages = await fetchInvoicesPages(query);
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -16,11 +23,11 @@ const Invoices = async () => {
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
-      </Suspense> */}
+      </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
